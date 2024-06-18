7634d337c7370759ef62373b1865003e5739daa2
Vincent Escoffier <alainvincentesc@gmail.com>
2024-06-18 16:13:49 +0200

    feat: init project

---
+++ src/main/java/org/example/lacentrale/LacentraleApplication.java
@@ -0,0 +1,13 @@
+package org.example.lacentrale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacentraleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LacentraleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lacentrale/LacentraleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lacentrale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacentraleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
